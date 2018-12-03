5e90e2f39196de8e52a91a45cc25d4851d61b541
alibrahimi95 <ilabrahimi13@gmail.com>
2018-12-03 16:24:43 +0100

    first commit

---
+++ src/com/example/allii/AproposActivity.java
@@ -0,0 +1,22 @@
+package com.example.allii;
+
+import com.example.ali.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class AproposActivity extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apropos);
+    }
+
+    
+}
